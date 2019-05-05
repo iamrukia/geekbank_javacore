a920a0ad1e644546dd64e3f0163734481a29125e
Nan Sha <iamshanan@gmail.com>
2019-05-06 00:36:54 +0100

    priorityqueue

---
+++ src/main/java/ocp/exam/guide/chap6/backed/collections/ArrayDemo.java
@@ -0,0 +1,20 @@
+package ocp.exam.guide.chap6.backed.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        myList.add("a");
+        myList.add("b");
+        myList.add("c");
+
+        String[] myArray = myList.toArray(new String[0]);
+
+        myList.add("hi");
+        System.out.println("myList: " + myList);
+        for (String item: myArray)
+            System.out.println("myArray: " + item);
+    }
+}
